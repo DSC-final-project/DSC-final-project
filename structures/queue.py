3c83c6b9401b4dabe95d665fe3610c25959348fe
Han <jaeyoon@g.skku.edu>
2025-06-09 19:50:54 +0900

    structures updated

--- structures/queue.py
+++ structures/queue.py
@@ -1,2 +1,25 @@
+from collections import deque
+
 class Queue:
-    
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, order):
+        self.queue.append(order)
+
+    def dequeue(self):
+        if self.queue:
+            return self.queue.popleft()
+        return None
+
+    def peek(self):
+        if self.queue:
+            return self.queue[0]
+        return None
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def __len__(self):
+        return len(self.queue)
+ 
